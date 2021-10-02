1f3accaa7a551c0ebe5b7c718cb8a563cf202bdd
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2021-10-02 18:11:46 +0300

    JSX Koşul Ifadeleri

--- Hello-react/public/scripts/app.js
+++ Hello-react/public/scripts/app.js
@@ -1,31 +1,31 @@
 "use strict";
 
 // JSX - Javascript XML
-var root = document.getElementById('root');
+var root = document.getElementById("root");
 var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", {
   id: "header"
 }, "Hello World"), /*#__PURE__*/React.createElement("div", null, "Lorem, ipsum dolor."), /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum.")));
 var name = "Samsung S10";
 var price = 5000;
-var description = "çok iyi bir telefon";
 var product = {
-  name: "Samsung S11",
+  name: "Samsung S12",
   price: 7000,
-  description: "çok iyi bir telefon"
+  desc: "iyi bir telefon",
+  types: ["red", "blue"]
 };
 
-function formatPrice(p) {
-  return p.price + ' TL';
+function getDesc(desc) {
+  if (desc) {
+    return /*#__PURE__*/React.createElement("p", {
+      id: "product-desc"
+    }, "description :", desc);
+  }
 }
 
 var template2 = /*#__PURE__*/React.createElement("div", {
   id: "product-details"
 }, /*#__PURE__*/React.createElement("h2", {
   id: "product-name"
-}, "name: ", product.name), /*#__PURE__*/React.createElement("p", {
-  id: "product-price"
-}, "price: ", formatPrice(product)), /*#__PURE__*/React.createElement("p", {
-  id: "product-desc"
-}, "description: ", product.description)); // ReactDOM
+}, "name: ", product.name ? product.name : "no name"), product.price && product.price > 0 && /*#__PURE__*/React.createElement("p", null, " price: ", product.price, " TL "), getDesc(product.desc), /*#__PURE__*/React.createElement("p", null, " ", product.types.length > 0 ? "there are options" : "no options", " ")); // ReactDOM
 
 ReactDOM.render(template2, root);
--- Hello-react/src/app.js
+++ Hello-react/src/app.js
@@ -1,36 +1,42 @@
 // JSX - Javascript XML
-var root = document.getElementById('root');
+var root = document.getElementById("root");
 
-var template =  <div>
-                    <h1 id="header">Hello World</h1>
-                    <div>Lorem, ipsum dolor.</div>
-                    <ul>
-                        <li>Lorem, ipsum.</li>
-                        <li>Lorem, ipsum.</li>
-                        <li>Lorem, ipsum.</li>
-                    </ul>
-                </div>;
+var template = (
+  <div>
+    <h1 id="header">Hello World</h1>
+    <div>Lorem, ipsum dolor.</div>
+    <ul>
+      <li>Lorem, ipsum.</li>
+      <li>Lorem, ipsum.</li>
+      <li>Lorem, ipsum.</li>
+    </ul>
+  </div>
+);
 
 var name = "Samsung S10";
 var price = 5000;
-var description = "çok iyi bir telefon"
 
 var product = {
-    name : "Samsung S11",
-    price : 7000,
-    description : "çok iyi bir telefon"
-}
+  name: "Samsung S12",
+  price: 7000,
+  desc: "iyi bir telefon",
+  types: ["red", "blue"],
+};
 
-function formatPrice(p) {
-    return p.price + ' TL';
+function getDesc(desc) {
+  if (desc) {
+    return <p id="product-desc">description :{desc}</p>;
+  }
 }
 
 var template2 = (
-    <div id="product-details">
-        <h2 id="product-name">name: {product.name}</h2>
-        <p id="product-price">price: {formatPrice(product)}</p>
-        <p id="product-desc">description: {product.description}</p>
-    </div>
+  <div id="product-details">
+    <h2 id="product-name">name: {product.name ? product.name : "no name"}</h2>
+    {/* <p id="product-price">price: {product.price== 0 ?'free':product.price}</p> */}
+    {product.price && product.price > 0 && <p> price: {product.price} TL </p>}
+    {getDesc(product.desc)}
+    <p> {product.types.length > 0 ? "there are options" : "no options"} </p>
+  </div>
 );
 
 // ReactDOM
