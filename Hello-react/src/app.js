56fbb48e1d5fd8384730597f05529dc117cb1b1a
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2021-10-03 09:40:11 +0300

    Data Binding

--- Hello-react/src/app.js
+++ Hello-react/src/app.js
@@ -17,27 +17,40 @@ var number = 0;
 var btnOneClassName = "btnRed";
 var btnOneMınusClassNAme = "btnBlue";
 function addOne() {
+  number++;
+  renderApp();
   console.log("ad one");
 }
 var minusOne = () => {
+  number--;
+  renderApp();
   console.log("minus one");
 };
+function renderApp() {
+  var template2 = (
+    <div>
+      <h1> Number : {number} </h1>
+      <button className={btnOneClassName} onClick={addOne}>
+        +1{" "}
+      </button>
+      <button className={btnOneMınusClassNAme} onClick={minusOne}>
+        -1{" "}
+      </button>
+    </div>
+  );
+  ReactDOM.render(template2, root);
+}
 
-var template2 = (
-  <div>
-    <h1> Number : {number} </h1>
-    <button className={btnOneClassName} onClick={addOne}>
-      +1{" "}
-    </button>
-    <button
-      className={btnOneMınusClassNAme}
-      onClick={() => {
-        console.log("minus one");
-      }}
-    >
-      -1{" "}
-    </button>
-  </div>
-);
+function tick() {
+  var element = (
+    <div>
+      <h2>time is : {new Date().toLocaleDateString()} </h2>
+    </div>
+  );
 
-ReactDOM.render(template2, root);
+  ReactDOM.render(element, root);
+}
+setInterval(() => {
+  tick();
+}, 1000);
+// renderApp();
